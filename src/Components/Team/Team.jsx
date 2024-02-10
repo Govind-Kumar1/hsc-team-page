d68bbd695c0b4399e47b662d33947dfec057f95f
Govind-Kumar1 <136782602+Govind-Kumar1@users.noreply.github.com>
2024-02-10 11:26:03 +0530

    initial

---
+++ src/Components/Team/Team.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import img1 from './download.jpeg'
+import './Team.css'
+const Team = () => {
+  return (
+    <div className='team'>
+      <h1>TEAMS</h1>
+      <img src={img1} alt=''></img>
+    </div>
+  )
+}
+
+export default Team
